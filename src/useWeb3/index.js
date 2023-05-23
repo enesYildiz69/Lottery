5480afbb94c592dcdf8a6411dc74e6bdc64ba01d
Enes Yildiz <enesyildizmr@gmail.com>
2023-05-23 22:17:18 +0300

    updated address

--- src/useWeb3/index.js
+++ src/useWeb3/index.js
@@ -1,5 +1,5 @@
 const { my_lottery_abi } = require('../contracts');
 
-const contract_address = "0xDfdBca11522b5A15738905E5059681B4e63312C5";
+const contract_address = "0x1cFE2f6b645831C3139Fb6e06F569E0E6Ae70392";
 
 export { contract_address, my_lottery_abi };
